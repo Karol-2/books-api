fb7cff8877c3bf2681f0d6f6636a6f7d619f488a
Karol-2 <97386279+Karol-2@users.noreply.github.com>
2024-09-28 17:15:16 +0200

    Add book controller

---
+++ src/main/java/com/example/booksapi/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.example.booksapi.controllers;
+
+import com.example.booksapi.models.Book;
+import com.example.booksapi.services.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService){
+        this.bookService = bookService;
+    }
+    @GetMapping
+    public List<Book> getAll(){
+        return this.bookService.getAllBooks();
+    }
+}
